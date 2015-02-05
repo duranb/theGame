039a9db9591408c01719e1ddd20ca2052ae0dd3b
Vaklav <jorgegarridor@gmail.com>
2015-02-04 23:30:50 -0800

    Added player tag Created HGInvestigatorbehaviour, to control general investigator behaviors Created HGCharacterController to wrap all character input and physics behavior Created HGPlayerTrackingCamera to dynamically control the camera to track the player

---
+++ Assets/Resources/Scripts/Actors/HGInvestigatorBehaviour.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class HGInvestigatorBehaviour : MonoBehaviour
+{
+	// movement config
+	public float _gravity = -25f;
+	public float _runSpeed = 8f;
+	public float _groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float _inAirDamping = 5f;
+	public float _jumpHeight = 3f;
+
+	[HideInInspector]
+	private float _normalizedHorizontalSpeed = 0;
+
+	private HGCharacterController _controller;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
+
+
+
+
+	void Awake()
+	{
+		_controller = GetComponent<HGCharacterController>();
+
+		// listen to some events for illustration purposes
+		_controller.OnControllerCollidedEvent += OnControllerCollider;
+		_controller.OnTriggerEnterEvent += OnTriggerEnterEvent;
+		_controller.OnTriggerExitEvent += OnTriggerExitEvent;
+	}
+
+
+	#region Event Listeners
+
+	void OnControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
+
+
+	void OnTriggerEnterEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
+	}
+
+
+	void OnTriggerExitEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+
+	#endregion
+
+
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller._velocity;
+
+		if( _controller._isGrounded )
+			_velocity.y = 0;
+
+		if( Input.GetKey( KeyCode.RightArrow ) )
+		{
+			_normalizedHorizontalSpeed = 1;
+			if( transform.localScale.x < 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+		}
+		else if( Input.GetKey( KeyCode.LeftArrow ) )
+		{
+			_normalizedHorizontalSpeed = -1;
+			if( transform.localScale.x > 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+		}
+		else
+		{
+			_normalizedHorizontalSpeed = 0;
+		}
+
+
+		// we can only jump whilst grounded
+		if( _controller._isGrounded && Input.GetKeyDown( KeyCode.UpArrow ) )
+		{
+			_velocity.y = Mathf.Sqrt( 2f * _jumpHeight * -_gravity );
+		}
+
+
+		// apply horizontal speed smoothing it
+		var smoothedMovementFactor = _controller._isGrounded ? _groundDamping : _inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, _normalizedHorizontalSpeed * _runSpeed, Time.deltaTime * smoothedMovementFactor );
+
+		// apply gravity before moving
+		_velocity.y += _gravity * Time.deltaTime;
+
+		_controller.move( _velocity * Time.deltaTime );
+	}
+
+}
